e1c4c64dbee8f79649e4f70b6c670131785cfc96
Sami Sellami <s.sellami@innopolis.university>
2021-10-20 10:25:12 +0300

    test view

---
+++ src/backend/dispatchapp/notification.py
@@ -0,0 +1,9 @@
+notification = [
+    {
+        'name': 'Sellami',
+        'Surname': 'Sami',
+        'email': 's.sellami@innopolis.university',
+        'phone': 9600606996,
+
+    }
+]
--- src/backend/dispatchapp/views.py
+++ src/backend/dispatchapp/views.py
@@ -1,3 +1,16 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .notification import notification
 # Create your views here.
+
+
+def test(request):
+    return JsonResponse('Hello', safe=False)
+
+
+class DispatchAPIView(APIView):
+
+    def get(self, request):
+        return Response(notification)
